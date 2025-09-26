08970452f88de3fb3e74aa81092cadd198f3aa0d
ryokasb <dant.riverar@duocuc.cl>
2025-09-25 21:03:03 -0300

    arreglos

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -7,3 +7,36 @@ if(!usuarioActivo){
     // mostrar contenido de usuario
     document.getElementById("user").innerText = `${usuarioActivo}`;
 }
+
+const form = document.getElementById("form-contacto");
+
+//coloca el nombre del usuario asignado
+document.getElementById("nombre").value= `${usuarioActivo}`;
+
+// Manejar el envío
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    const nombre = form.nombre.value.trim();
+    const correo = form.correo.value.trim();
+    const mensaje = form.mensaje.value.trim();
+   
+    
+
+    // Validar campos
+    if (nombre === "" || correo === "" || mensaje === "") {
+        alert("Por favor completa todos los campos.");
+        return;
+    }
+
+    // Validar correo simple
+    if (!correo.includes("@") || !correo.includes(".")) {
+        alert("Ingresa un correo válido.");
+        return;
+    }
+
+    // Mensaje de éxito
+    alert("¡Mensaje enviado! Te responderemos pronto.");
+
+    // Limpiar formulario
+    form.reset();
+});
